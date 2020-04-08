3b2e35d753ebd4a5e2e6eed9f288dc626efdad45
OrlandoMatteo <matteo.orlando.1993@gmail.com>
2020-04-08 15:27:10 +0200

    Created page for layer and compare and modified index

--- js/map.js
+++ js/map.js
@@ -2,3 +2,12 @@ var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(mymap);
+
+function collapseMap(){
+    if ($('#mapid')[0].style.height!='0vh'){
+        $('#mapid')[0].style.height='0vh';
+    }
+    else{
+        $('#mapid')[0].style.height='50vh';
+    }
+}
